7bbf4d9237f3680429b66be68c321264b8058900
surezluvy <raynaldizoel@gmail.com>
2022-09-04 17:14:55 +0700

    Mengerjakan Fragment Chat

--- app/src/main/java/com/example/gojekduplicate/MainActivity.kt
+++ app/src/main/java/com/example/gojekduplicate/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.gojekduplicate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+     supportFragmentManager.beginTransaction().apply {
+         replace(R.id.fragmentContainerGojek,ChatFragment())
+         commit()
+     }
+        bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.menuHome ->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerGojek,HomeFragment()).commit()
+                    return@setOnNavigationItemSelectedListener true
+                } R.id.menuPromos ->{
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerGojek,PromosFragment()).commit()
+                return@setOnNavigationItemSelectedListener true
+                } R.id.menuOrders ->{
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerGojek,OrdersFragment()).commit()
+                return@setOnNavigationItemSelectedListener true
+                } R.id.menuChat ->{
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerGojek,ChatFragment()).commit()
+                return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
